e7b2d12884b2b2de574cf7130ac89fa6934cdbb0
literally_weird <55377506+volohan@users.noreply.github.com>
2021-03-08 22:17:50 +0500

    task_1

--- task_1.py
+++ task_1.py
@@ -1,35 +1,28 @@
-def task_1(number):
-    adjacency_matrix = []
+adjacency_matrix = []
 
-    with open(f'task_1_tests\\{number}.txt', 'r') as graph:
-        N = int(graph.readline())
-        for _ in range(N):
-            vertex = list(map(int, graph.readline().split(' ')))
-            adjacency_matrix.append(vertex)
-        print(f"[{graph.readline()}]")
+with open('test.txt', 'r') as graph:
+    N = int(graph.readline())
+    for _ in range(N):
+        vertex = list(map(int, graph.readline().split(' ')))
+        adjacency_matrix.append(vertex)
 
-    if N > 1:
-        res = True
-        for head in range(N):
-            search = [(head, {head})]
-            while len(search):
-                vertex_index, visited = search.pop()
-                vertex = adjacency_matrix[vertex_index]
-                for i in range(N):
-                    if vertex[i] == 1:
-                        if i == head:
-                            if len(visited) % 2 != 0:
-                                res = False
-                        elif i not in visited:
-                            new = set(visited)
-                            new.add(i)
-                            search.append((i, new))
-    else:
-        res = False
+if N > 1:
+    res = 'Y'
+    for head in range(N):
+        search = [(head, {head})]
+        while len(search):
+            vertex_index, visited = search.pop()
+            vertex = adjacency_matrix[vertex_index]
+            for i in range(N):
+                if vertex[i] == 1:
+                    if i == head:
+                        if len(visited) % 2 != 0:
+                            res = 'N'
+                    elif i not in visited:
+                        new = set(visited)
+                        new.add(i)
+                        search.append((i, new))
+else:
+    res = 'N'
 
-    print(res)
-    print("-----------------------------------------------------------------")
-
-
-for i in range(6):
-    task_1(i)
+print(res)
