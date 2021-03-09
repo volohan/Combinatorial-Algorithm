49b2661e63e8b4d1a7e887df0544640a9a72ab70
literally_weird <55377506+volohan@users.noreply.github.com>
2021-03-09 10:15:40 +0500

    task_1

--- task_1.py
+++ task_1.py
@@ -1,6 +1,6 @@
 adjacency_matrix = []
 
-with open('test.txt', 'r') as graph:
+with open('in.txt', 'r') as graph:
     N = int(graph.readline())
     for _ in range(N):
         vertex = list(map(int, graph.readline().split(' ')))
@@ -8,21 +8,32 @@ with open('test.txt', 'r') as graph:
 
 if N > 1:
     res = 'Y'
-    for head in range(N):
-        search = [(head, {head})]
-        while len(search):
-            vertex_index, visited = search.pop()
-            vertex = adjacency_matrix[vertex_index]
-            for i in range(N):
-                if vertex[i] == 1:
-                    if i == head:
-                        if len(visited) % 2 != 0:
-                            res = 'N'
-                    elif i not in visited:
-                        new = set(visited)
-                        new.add(i)
-                        search.append((i, new))
+    fraction_one = {0}
+    fraction_two = set()
+    search = [(0, {0})]
+    while len(search):
+        vertex_index, visited = search.pop()
+        vertex = adjacency_matrix[vertex_index]
+        for i in range(N):
+            if vertex[i] == 1:
+                if len(visited) % 2 == 0:
+                    if i in fraction_two:
+                        res = 'N'
+                        break
+                    fraction_one.add(i)
+                else:
+                    if i in fraction_one:
+                        res = 'N'
+                        break
+                    fraction_two.add(i)
+                if i not in visited:
+                    new = set(visited)
+                    new.add(i)
+                    search.append((i, new))
+        if res == 'N':
+            break
 else:
     res = 'N'
 
-print(res)
+with open('out.txt', 'w') as file:
+    file.write(res)
